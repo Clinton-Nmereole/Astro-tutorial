82cb77895e4561d8e7f7e023f726f4fdda9321ef
Clinton Nmereole <clintonnmereole@gmail.com>
2023-05-28 17:01:16 -0500

    changing memoji to more centered image

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,9 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+               width: {
+                    '112' : '28rem',
+               }
+          },
 	},
-	plugins: [
-          require('flowbite/plugin')
-     ],
+	plugins: [],
 }
