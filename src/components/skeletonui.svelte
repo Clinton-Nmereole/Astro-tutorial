2e8d641ef49416c1ff008b28e6b16a2963946198
Clinton Nmereole <clintonnmereole@gmail.com>
2023-08-01 21:03:44 -0500

    adding projects section

--- src/components/skeletonui.svelte
+++ src/components/skeletonui.svelte
@@ -1,14 +1,14 @@
 <script lang='ts'>
 // Your selected Skeleton theme:
-  import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
 
 // This contains the bulk of Skeletons required styles:
-  import '@skeletonlabs/skeleton/styles/skeleton.css';
+import '@skeletonlabs/skeleton/styles/skeleton.css';
 
 // Finally, your application's global stylesheet (sometimes labeled 'app.css')
-  import '../styles/global.css';
+import '../styles/global.css';
 
-  import { AppShell, Appbar } from '@skeletonlabs/skeleton';
+import { ProgressBar } from '@skeletonlabs/skeleton';
 </script>
 
-
+<ProgressBar label="Progress Bar" value={50} max={100} />
