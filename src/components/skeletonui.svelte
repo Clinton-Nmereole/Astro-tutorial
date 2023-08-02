e1833614a94ba91e46175cbc68fb614e647ac291
Clinton Nmereole <clintonnmereole@gmail.com>
2023-08-01 20:48:57 -0500

    adding projects section

--- src/components/skeletonui.svelte
+++ src/components/skeletonui.svelte
@@ -6,5 +6,19 @@
   import '@skeletonlabs/skeleton/styles/skeleton.css';
 
 // Finally, your application's global stylesheet (sometimes labeled 'app.css')
-  import '../app.postcss';
+  import '../styles/global.css';
+
+  import { AppShell, Appbar } from '@skeletonlabs/skeleton';
 </script>
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-3xl uppercase text-secondary-400">Test</strong>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+</AppShell>
