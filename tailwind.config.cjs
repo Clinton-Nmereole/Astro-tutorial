9a5d539dcce991c184da1251a2bcf19f26d73691
Clinton Nmereole <clintonnmereole@gmail.com>
2023-08-01 19:42:16 -0500

    adding projects section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,39 +1,54 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-               width: {
-                    '100' : '25rem',
-                    '104' : '26rem',
-                    '112' : '28rem',
-               },
-               colors: {
-                    transparent: 'transparent',
-                    current: 'currentColor',
-                    'white': '#ffffff',
-                    'purple': '#3f3cbb',
-                    'midnight': '#121063',
-                    'metal': '#565584',
-                    'tahiti': '#3ab7bf',
-                    'silver': '#ecebff',
-                    'bubble-gum': '#ff77e9',
-                    'bermuda': '#78dcca',
-                    'tan': '#c09580',
-                    'blackboard':'#565656',
-                    'darkgrain':"#ACA49F",
-                    'grain':"#d7cec7"
-               },
-               fontFamily: {
-                    'Stardom': 'Stardom-Regular',
-                    'Chillax':['Chillax-Regular', 'Chillax-Medium', 'Chillax-Semibold', 'Chillax-Bold', 'Chillax-Light', 'Chillax-Extralight', 'Chillax-Variable'],
-                    'Array': ['Array-Regular', 'Array-Semibold', 'Array-Bold'],
-               },
-               spacing : {
-                    '3.5': '0.845rem',
-                    '1,5': '0.375rem',
-               }
-          },
-	},
-	plugins: [],
-}
+  darkMode: "class",
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    require("path").join(
+      require.resolve("@skeletonlabs/skeleton"),
+      "../**/*.{html,js,svelte,ts}"
+    ),
+  ],
+  theme: {
+    extend: {
+      width: {
+        100: "25rem",
+        104: "26rem",
+        112: "28rem",
+      },
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        white: "#ffffff",
+        purple: "#3f3cbb",
+        midnight: "#121063",
+        metal: "#565584",
+        tahiti: "#3ab7bf",
+        silver: "#ecebff",
+        "bubble-gum": "#ff77e9",
+        bermuda: "#78dcca",
+        tan: "#c09580",
+        blackboard: "#565656",
+        darkgrain: "#ACA49F",
+        grain: "#d7cec7",
+      },
+      fontFamily: {
+        Stardom: "Stardom-Regular",
+        Chillax: [
+          "Chillax-Regular",
+          "Chillax-Medium",
+          "Chillax-Semibold",
+          "Chillax-Bold",
+          "Chillax-Light",
+          "Chillax-Extralight",
+          "Chillax-Variable",
+        ],
+        Array: ["Array-Regular", "Array-Semibold", "Array-Bold"],
+      },
+      spacing: {
+        3.5: "0.845rem",
+        "1,5": "0.375rem",
+      },
+    },
+  },
+  plugins: [require("@skeletonlabs/skeleton/tailwind/skeleton.cjs")()],
+};
